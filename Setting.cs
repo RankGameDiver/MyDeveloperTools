28de70e9ce6ac4663b2797cca8126b30ed03b913
RankGameDiver <hoo2018@naver.com>
2020-03-02 12:52:24 +0900

    세팅 관련 코드 추가

---
+++ Setting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Setting : MonoBehaviour
+{
+    public int maxFrame = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        SetSetting();
+    }
+
+    public void SetSetting()
+    {
+        if (maxFrame > 0) Application.targetFrameRate = maxFrame;
+    }
+}
