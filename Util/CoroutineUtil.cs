5186d3d39c09ad6228c7cdc7f493bb15419179fb
RankGameDiver <hoo2018@naver.com>
2020-03-02 15:47:18 +0900

    CoroutineUtil 이동

---
+++ Util/CoroutineUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CoroutineUtil
+{
+    public static IEnumerator Move(this GameObject obj, Vector3 endPos, float time)
+    {
+        float t = 0f;
+        while (t < 1f)
+        {
+            t += Time.deltaTime / time;
+            if (t > 1f) t = 1f;
+
+            obj.transform.position =  Vector3.Lerp(obj.transform.position, endPos, t);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
