4f7655329c2facb46c67cfc85458f8d56395b6f1
RankGameDiver <hoo2018@naver.com>
2020-03-03 12:35:27 +0900

    MonoSingleton 추가

---
+++ Util/MonoSingleton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T st_instance;
+
+    public static T Instance
+    {
+        get 
+        {
+            if (st_instance == null)
+            {
+                T i = FindObjectOfType<T>();
+
+                if (i == null)
+                    Debug.LogError("scene안에 해당하는 컴포넌트가 없습니다. name L " + typeof(T).Name);
+                else 
+                    st_instance = i;
+            }
+            return st_instance;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        st_instance = null;
+    }
+}
