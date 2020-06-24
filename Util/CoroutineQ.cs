ac1c21df27cd467ba98a2f3446b12111d6643280
RankGameDiver <hoo2018@naver.com>
2020-06-24 14:46:58 +0900

    CoroutineQ 기능 업데이트

--- Util/CoroutineQ.cs
+++ Util/CoroutineQ.cs
@@ -7,24 +7,49 @@ public class CoroutineQ : MonoBehaviour
     private Queue<IEnumerator> m_Queue = new Queue<IEnumerator>();
     private Coroutine curCo = null;
 
+    [SerializeField]
+    private bool bPlay = false;
+    [SerializeField]
+    private int m_Count = 0;
+
     public void Enqueue(IEnumerator co)
     {
         m_Queue.Enqueue(co);
+        m_Count = m_Queue.Count;
     }
 
     public void Play(IEnumerator co = null)
     {
         if (co != null) Enqueue(co);
 
-        if (m_Queue.Count == 0) return;
+        if (m_Queue.Count == 0 || bPlay) return;
 
+        if (m_Queue.Count == 0)
+        {
+            Debug.Log("[CoroutineQ] Data count is null");
+            return;
+        }
+        else if (bPlay)
+        {
+            Debug.Log("[CoroutineQ] Queue is playing");
+            return;
+        }
+
+        bPlay = true;
         StartCoroutine(PlayRoutine());
     }
 
     public void Cancle()
     {
-        StopCoroutine(curCo);
+        if (curCo != null) StopCoroutine(curCo);
+        Clear();
+    }
+
+    private void Clear()
+    {
+        bPlay = false;
         m_Queue.Clear();
+        m_Count = 0;
     }
 
     private IEnumerator PlayRoutine()
@@ -32,9 +57,15 @@ public class CoroutineQ : MonoBehaviour
         while (m_Queue.Count > 0)
         {
             curCo = StartCoroutine(m_Queue.Dequeue());
+            m_Count = m_Queue.Count;
             yield return curCo;
         }
-
+        Clear();
         yield return null;
+    }
+
+    public int GetCount()
+    {
+        return m_Count;
     }
 }
